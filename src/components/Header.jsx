926ecfacd1a4632526a889f627176efe4cbc9177
Mahesh <mj7267889@gmail.com>
2023-12-10 15:56:21 +0530

    design_changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = ({}) => {
     <>
       <nav class="navbar navbar-expand-lg bg-primary">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#"> <h2 className="text-white">Elite Teams - Hire Teams</h2></a>
+          <a class="navbar-brand" href="#"> <h4 className="text-white">Elite Teams - Hire Teams</h4></a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
